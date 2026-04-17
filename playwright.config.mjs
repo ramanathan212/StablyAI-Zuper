c8b070d2a7ee2ee08c9dd0f8107b29263df49e27
Ragu1108 <ragupathys1103@gmail.com>
2026-04-17 14:55:10 +0530

    Added the filterfile in the Jobs suite

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -153,7 +153,7 @@ export default defineConfig({
     {
       name: 'Jobs',
       grep: /(Jobs Bulk Update Fields should bulk update all specified fields for selected jobs|Jobs Filter Operator Workflow should filter jobs by multiple filters with all available operators and print row counts)$/i,
-      testMatch: ['jobs-bulk-update-fields.spec.ts'],
+      testMatch: ['jobs-bulk-update-fields.spec.ts','jobs-filter-operators.spec.ts'],
       use: {
         ...devices['Desktop Chrome'],
       },
